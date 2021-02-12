8298bb3cf7921d6d144ab53b0a2de8da4e4130bc
Ilya Pashchenko <in4pashchenko@gmail.com>
2021-02-12 21:41:43 +0300

    Removed unused functions.

--- include/utils.h
+++ include/utils.h
@@ -21,10 +21,8 @@ const double m_e = 9.10938356e-28;
 const double m_p = 1.672621898e-24;
 // Charge of electron [C]
  const double q_e = 4.80320467299766e-10;
-//const double q_e = 1.6*1E-19;
 // Charge of proton [C]
  const double q_p = q_e;
-//const double q_p = 1.6*1E-19;
 // Speed of light [cm / s]
 const double c = 29979245800.0;
 // Gravitational constant
@@ -54,108 +52,6 @@ double nu_min(double gamma_min, Vector3d &b, Vector3d &n_los);
 
 double nu_min(double gamma_min, double b);
 
-// For random B-field
-double k_0(double b, Vector3d &n_los, double nu, double n);
-
-// For vector B-field
-double k_0(Vector3d &b, Vector3d &n_los, double nu, double n);
-
-double k_0_value(Vector3d &b, double nu, double n);
-
-// For random B-field
-double k_i(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-// Absorption coefficient for given vector of magnetic field ``b``, unit LOS
-// vector ``n_los`` and others measured in emission frame
-double k_i(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double eta_0(double b, Vector3d &n_los, double n);
-
-// For vector B-field
-double eta_0(Vector3d &b, Vector3d &n_los, double n);
-
-double eta_0_value(Vector3d &b, double n);
-
-// For random B-field
-double eta_i(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-// Emission coefficient for given vector of magnetic field ``b``, unit LOS
-// vector ``n_los`` and others measured in emission frame
-double eta_i(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double k_q(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double k_q(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double k_u(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double k_u(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double k_v(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double k_v(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double k_F_c(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double k_F_c(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double k_C_c(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double k_C_c(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-// TODO: For e+ ``k_F_r`` will have different sign, but ``k_C_r`` - the same
-// sign
-double k_F_r(Vector3d &b, Vector3d &n_los, double nu, double n,
-             double gamma_min=100., double s=2.5);
-
-// For random B-field
-double k_F_r(double b, Vector3d &n_los, double nu, double n,
-             double gamma_min=100., double s=2.5);
-
-// For vector B-field
-double k_C_r(Vector3d &b, Vector3d &n_los, double nu, double n,
-             double gamma_min=100., double s=2.5);
-
-// For random B-field
-double k_C_r(double b, Vector3d &n_los, double nu, double n,
-             double gamma_min=100., double s=2.5);
-
-// For vector B-field
-double h_Q(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double h_Q(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For vector B-field
-double eta_q(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double eta_q(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-double eta_u(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double eta_u(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-double eta_v(Vector3d &b, Vector3d &n_los, double nu, double n, double s=2.5);
-
-// For random B-field
-double eta_v(double b, Vector3d &n_los, double nu, double n, double s=2.5);
-
 // Lorentz factor for the velocity ``v``
 double getG(Vector3d &v);
 
@@ -181,20 +77,7 @@ Vector3d q(Vector3d &b_hat, Vector3d &v_beta, Vector3d &n_hat);
 
 Vector3d e_hat(Vector3d &b_hat, Vector3d &v_beta, Vector3d &n_hat);
 
-
-class Ctd {
-public:
-	//From astropy.cosmology.WMAP9
-	Ctd(double z, double H0=69.32, double omega_M=0.2865, double omega_V=0.7134130719051658,
-			double gamma_nu=8.69280948342326e-05);
-	void operator() (const double &x, double &dxdt, const double t);
-	double z;
-	double H0;
-	double omega_M;
-	double omega_V;
-	double gamma_nu;
-};
-
+// This uses adaptive integration as the Astropy version with the same tolerances.
 double comoving_transfer_distance2(double z, double H0=69.32, double omega_M=0.2865, double omega_V=0.7134130719051658, double gamma_nu=8.69280948342326e-05);
 
 // Return scale factor that converts from parsecs to milliarcseconds
--- src/utils.cpp
+++ src/utils.cpp
@@ -32,113 +32,6 @@ double nu_b_value(Vector3d &b) {
 	return q_e*b.norm()/(2.*pi*m_e*c);
 }
 
-double k_0(Vector3d &b, Vector3d &n_los, double nu, double n) {
-    return pi*nu_p(n)*nu_p(n)*nu_b(b, n_los)/(c*nu*nu);
-}
-
-double k_0(double b, Vector3d &n_los, double nu, double n) {
-	return pi*nu_p(n)*nu_p(n)*nu_b(b)/(c*nu*nu);
-}
-
-double k_0_value(Vector3d &b, double nu, double n) {
-	return pi*nu_p(n)*nu_p(n)*nu_b_value(b)/(c*nu*nu);
-}
-
-double k_i(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-    double factor = (pow(3., (s+1.)/2.)/4.)*tgamma(s/4.+11./6.)*tgamma(s/4.+1./6.);
-    return k_0(b, n_los, nu, n) * pow(nu_b(b, n_los)/nu, s/2.) * factor;
-}
-
-double k_i(double b, Vector3d &n_los, double nu, double n, double s) {
-	double factor = (pow(3., (s+1.)/2.)/4.)*tgamma(s/4.+11./6.)*tgamma(s/4.+1./6.);
-	double rnd_factor = sqrt(pi/4.)*tgamma((6.+s)/4.)/tgamma((8.+s)/4.);
-	factor = factor*rnd_factor;
-	return k_0(b, n_los, nu, n) * pow(nu_b(b)/nu, s/2.) * factor;
-}
-
-// Alternative formulation
-double k_i_(double b, Vector3d &n_los, double nu, double n, double s) {
-	double alpha = (s-1.)/2.;
-	double factor = sqrt(pi/4.)*tgamma((7.+2.*alpha)/4.)/tgamma((9.+2.*alpha)/4.);
-	factor = factor*pow(m_e*c*c, 2.*alpha)*(sqrt(3.)*pow(q_e, 3.)/(8.*pi*m_e));
-	factor = factor*pow((3.*q_e)/(2.*pi*pow(m_e, 3.)*pow(c, 5.)), alpha+0.5);
-	factor = factor*tgamma((6.*alpha+5.)/12.)*tgamma((6.*alpha+25.)/12.);
-	return factor*pow(b, alpha+1.5)*n*pow(nu, -alpha-2.5);
-}
-
-double k_q(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return (s+2.)/(s+10./3)*k_i(b, n_los, nu, n, s);
-}
-
-double k_u(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return 0;
-}
-
-double k_v(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	double cos_theta = b.dot(n_los)/b.norm();
-	double factor = pow(3., s/2.)*(s+3.)*(s+2.)/(4.*(s+1.))*tgamma(s/4.+11./12.)*tgamma(s/4.+7./12.);
-	return -k_0_value(b, nu, n)*cos_theta*pow(nu_b(b, n_los)/nu, (s+1.)/2.) * factor;
-}
-
-double k_F_c(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	double cos_theta = b.dot(n_los)/b.norm();
-	return 2.*pi*nu_p(n)*nu_p(n)*nu_b_value(b)*cos_theta/(c*nu*nu);
-}
-
-double k_C_c(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return -pi*nu_p(n)*nu_p(n)*nu_b(b, n_los)*nu_b(b, n_los)/(c*nu*nu*nu);
-}
-
-double k_F_r(Vector3d &b, Vector3d &n_los, double nu, double n, double gamma_min, double s) {
-	return (s+2.)*log(gamma_min)/((s+1.)*pow(gamma_min, s+1.))*k_F_c(b, n_los, nu, n, s);
-}
-
-double k_C_r(Vector3d &b, Vector3d &n_los, double nu, double n, double gamma_min, double s) {
-	return (2./(s-2.))*(pow(gamma_min, 2.-s) - pow(nu_b(b, n_los)/nu, (s-2.)/2.))*k_C_c(b, n_los, nu, n, s);
-}
-
-double h_Q(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return 0;
-}
-
-double eta_0(Vector3d &b, Vector3d &n_los, double n) {
-    return pi*nu_p(n)*nu_p(n)*nu_b(b, n_los)*m_e/c;
-}
-
-double eta_0(double b, Vector3d &n_los, double n) {
-	return pi*nu_p(n)*nu_p(n)*nu_b(b)*m_e/c;
-}
-
-double eta_0_value(Vector3d &b, double n) {
-	return pi*nu_p(n)*nu_p(n)*nu_b_value(b)*m_e/c;
-}
-
-double eta_i(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-    double factor = pow(3., s/2.)/(2.*(s+1))*tgamma(s/4.+19./12.)*tgamma(s/4.-1./12.);
-    return eta_0(b, n_los, n) * pow(nu_b(b, n_los)/nu, (s-1.)/2.) * factor;
-}
-
-double eta_i(double b, Vector3d &n_los, double nu, double n, double s) {
-	double factor = pow(3., s/2.)/(2.*(s+1))*tgamma(s/4.+19./12.)*tgamma(s/4.-1./12.);
-	double rnd_factor = sqrt(pi/4.)*tgamma((5.+s)/4.)/tgamma((7.+s)/4.);
-	factor = factor*rnd_factor;
-	return eta_0(b, n_los, n) * pow(nu_b(b)/nu, (s-1.)/2.) * factor;
-}
-
-double eta_q(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return (s+1.0)/(s+7./3.)*eta_i(b, n_los, nu, n, s);
-}
-
-double eta_u(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	return 0;
-}
-
-double eta_v(Vector3d &b, Vector3d &n_los, double nu, double n, double s) {
-	double cos_theta = b.dot(n_los)/b.norm();
-	double factor = pow(3., (s-1.)/2.)*(s+2.)/(2.*s)*tgamma(s/4.+2./3.)*tgamma(s/4.+1./3.);
-	return -eta_0_value(b, n)*cos_theta*pow(nu_b(b, n_los)/nu, s/2.) * factor;
-}
-
 double getG(Vector3d &v) {
     Vector3d beta = v/c;
     return sqrt(1./(1.- beta.squaredNorm()));
@@ -230,23 +123,15 @@ double mas_to_pc(double z) {
 std::ostream &
 write_2dvector(std::ostream &os, std::vector<std::vector<double>> &v,
 							 double scale) {
-	for (int i = 0; i < v.size(); ++i) {
-		for (int j = 0; j < v[i].size(); ++j) {
-			double value = v[i][j]/scale;
+	for (auto & i : v) {
+		for (double j : i) {
+			double value = j/scale;
 			os << value <<" ";
 		}
 		os<<"\n";
 	}
 	return os;
 }
-
-Ctd::Ctd(double z, double H0, double omega_M, double omega_V, double gamma_nu) : z(z), H0(H0), omega_M(omega_M), omega_V(omega_V), gamma_nu(gamma_nu) {}
-
-void Ctd::operator()(const double &x, double &dxdt, const double t) {
-	dxdt = pow((omega_M + (1.+t)*gamma_nu)*(1.+t)*(1.+t)*(1.+t)+omega_V, -0.5);
-	//dxdt = pow(omega_M*(1.+t*t*t)+omega_V, -0.5);
-};
-
 
 void read_from_txt(const std::string& fntxt, std::vector< std::vector<double> >& properties) {
     std::ifstream infile(fntxt);
